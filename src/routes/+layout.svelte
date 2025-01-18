4b8c2dc65e8c66d515c6883e6fd8b552bc9b68ae
Dai Vo <daidaivo2002@gmail.com>
2025-01-17 23:42:26 -0600

    add vercel analytic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
   import "../app.css";
   import '@fortawesome/fontawesome-free/css/all.min.css';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit'
+
+  injectAnalytics()
 </script>
 
 <slot />
